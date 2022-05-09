bb6922bded92a6ef67cf8325bf7a65ac64081203
macedoogui <macedoogui@gmail.com>
2022-05-09 20:57:47 -0300

    deploy

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectToDatabase() {
   mongoose
-    .connect('mongodb://localhost:27017/filmes-db', {
+    .connect(process.env.DATABASE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
